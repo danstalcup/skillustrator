076c5664812e0bd567be8720efd8c2161ebbbcf9
bjsawyer <bjsawyer@vt.edu>
2016-12-20 23:06:00 -0500

    Added auto-populated skills dropdown to modal

--- UI/src/app/components/add-person-skill/add-person-skill.component.ts
+++ UI/src/app/components/add-person-skill/add-person-skill.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import { MaterializeAction } from 'angular2-materialize';
 
+import { Skill } from '../../models/skill';
+import { SkillsService } from '../../services/skills.service';
+
 @Component({
   selector: 'app-add-person-skill',
   templateUrl: './add-person-skill.component.html',
   styleUrls: ['./add-person-skill.component.css']
 })
 export class AddPersonSkillComponent implements OnInit {
-  modalActions = new EventEmitter<string|MaterializeAction>();
+  modalActions = new EventEmitter<string | MaterializeAction>();
+  availableSkills: Skill[];
+  selectedSkill: Skill;
 
-  constructor() { }
+  constructor(private skillsService: SkillsService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.skillsService.getAll().subscribe(skills => this.availableSkills = skills);
+  }
 
   openModal() {
     this.modalActions.emit({action: 'modal', params: ['open']});
