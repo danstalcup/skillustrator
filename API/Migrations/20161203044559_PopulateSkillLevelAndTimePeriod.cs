83f3771863bc0d0d91ff775bf6c5967f8f44e225
wyntuition <wynvandev@gmail.com>
2017-05-24 18:36:59 -0400

    uncomment migration

--- API/Migrations/20161203044559_PopulateSkillLevelAndTimePeriod.cs
+++ API/Migrations/20161203044559_PopulateSkillLevelAndTimePeriod.cs
@@ -8,15 +8,15 @@ namespace app.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.Sql("insert into \"SkillLevel\" (\"Code\", \"Description\") values ('BEGINNER', 'Beginner')");
-            //migrationBuilder.Sql("insert into \"SkillLevel\" (\"Code\", \"Description\") values ('INTERMEDIATE', 'Intermediate')");
-            //migrationBuilder.Sql("insert into \"SkillLevel\" (\"Code\", \"Description\") values ('EXPERT', 'Expert')");
+            migrationBuilder.Sql("insert into \"SkillLevel\" (\"Code\", \"Description\") values ('BEGINNER', 'Beginner')");
+            migrationBuilder.Sql("insert into \"SkillLevel\" (\"Code\", \"Description\") values ('INTERMEDIATE', 'Intermediate')");
+            migrationBuilder.Sql("insert into \"SkillLevel\" (\"Code\", \"Description\") values ('EXPERT', 'Expert')");
                                                                                      
-            //migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('LESS_ONE_MONTH', 'Less Than One Month')");
-            //migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('ONE_TO_SIX_MONTHS', 'One to Six Months')");
-            //migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('SIX_TO_TWELVE_MONTHS', 'Six to Twelve Months')");
-            //migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('ONE_TO_TWO_YEARS', 'One to Two Years')");
-            //migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('MORE_TWO_YEARS', 'More Than Two Years')");
+            migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('LESS_ONE_MONTH', 'Less Than One Month')");
+            migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('ONE_TO_SIX_MONTHS', 'One to Six Months')");
+            migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('SIX_TO_TWELVE_MONTHS', 'Six to Twelve Months')");
+            migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('ONE_TO_TWO_YEARS', 'One to Two Years')");
+            migrationBuilder.Sql("insert into \"TimePeriod\" (\"Code\", \"Description\") values ('MORE_TWO_YEARS', 'More Than Two Years')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
